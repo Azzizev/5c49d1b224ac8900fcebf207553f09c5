6ec1d55bb946fe216f68378066fd56f1095a3e20
Martyrton <martins.ayrton37@gmail.com>
2018-04-20 15:56:51 +0200

    Aggiunta del layout new recipe

---
+++ app/src/main/java/cloud/dishwish/ragmart/dishwish/new_recipe/NewRecipeActivity.java
@@ -0,0 +1,15 @@
+package cloud.dishwish.ragmart.dishwish.new_recipe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cloud.dishwish.ragmart.dishwish.R;
+
+public class NewRecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_recipe_activity);
+    }
+}
